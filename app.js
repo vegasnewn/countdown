250163261b12eb45df1b70e6fda45256d8326198
Vegas Newn <vegas.newn@gmail.com>
2021-09-29 17:11:11 +1000

    added 3 more dates

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
+const halloweenId = document.getElementById('halloween')
+const grandma1Id = document.getElementById('grandma1')
 const xmasId = document.getElementById('xmas')
 const sookId = document.getElementById('sook')
-const grandmaId = document.getElementById('grandma')
+const grandma2Id = document.getElementById('grandma2')
 const leonId = document.getElementById('leon')
+const easterId = document.getElementById('easter')
 const vegasId = document.getElementById('vegas')
 const grandfaId = document.getElementById('grandfa')
 const jionId = document.getElementById('jion')
 
+const halloween = new Date('10/31/2021')
+const grandma1 = new Date('11/09/2021')
 const xmas = new Date('12/25/2021')
 const sook = new Date('01/14/2022')
-const grandma = new Date('03/08/2022')
+const grandma2 = new Date('03/08/2022')
 const leon = new Date('03/16/2022')
+const easter = new Date('04/17/2022')
 const vegas = new Date('07/17/2022')
 const grandfa = new Date('08/05/2022')
 const jion = new Date('09/22/2022')
@@ -26,17 +32,43 @@ function countDown(date) {
   return days
 }
 
+const halloweenDays = countDown(halloween)
+halloweenId.innerHTML = `Halloween is ${halloweenDays.toLocaleString('en-US', {
+  timeZone: 'Australia/Brisbane',
+})} days left.`
+const grandma1Days = countDown(grandma1)
+grandma1Id.innerHTML = `Grandmother's birthday is ${grandma1Days.toLocaleString('en-US', {
+  timeZone: 'Australia/Brisbane',
+})} days left.`
 const xmasDays = countDown(xmas)
-xmasId.innerHTML = `Christmas is ${xmasDays} days left.`
+xmasId.innerHTML = `Christmas is ${xmasDays.toLocaleString('en-US', {
+  timeZone: 'Australia/Brisbane',
+})} days left.`
 const sookDays = countDown(sook)
-sookId.innerHTML = `Myo Sook's birthday is ${sookDays} days left.`
-const grandmaDays = countDown(grandma)
-grandmaId.innerHTML = `Grandmother's birthday is ${grandmaDays} days left.`
+sookId.innerHTML = `Myo Sook's birthday is ${sookDays.toLocaleString('en-US', {
+  timeZone: 'Australia/Brisbane',
+})} days left.`
+const grandma2Days = countDown(grandma2)
+grandma2Id.innerHTML = `Grandmother's birthday is ${grandma2Days.toLocaleString('en-US', {
+  timeZone: 'Australia/Brisbane',
+})} days left.`
 const leonDays = countDown(leon)
-leonId.innerHTML = `Leon's birthday is ${leonDays} days left.`
+leonId.innerHTML = `Leon's birthday is ${leonDays.toLocaleString('en-US', {
+  timeZone: 'Australia/Brisbane',
+})} days left.`
+const easterDays = countDown(easter)
+easterId.innerHTML = `Easter is ${easterDays.toLocaleString('en-US', {
+  timeZone: 'Australia/Brisbane',
+})} days left.`
 const vegasDays = countDown(vegas)
-vegasId.innerHTML = `Vegas' birthday is ${vegasDays} days left.`
+vegasId.innerHTML = `Vegas' birthday is ${vegasDays.toLocaleString('en-US', {
+  timeZone: 'Australia/Brisbane',
+})} days left.`
 const grandfaDays = countDown(grandfa)
-grandfaId.innerHTML = `Grandfather's birthday is ${grandfaDays} days left.`
+grandfaId.innerHTML = `Grandfather's birthday is ${grandfaDays.toLocaleString('en-US', {
+  timeZone: 'Australia/Brisbane',
+})} days left.`
 const jionDays = countDown(jion)
-jionId.innerHTML = `Jion's birthday is ${jionDays} days left.`
+jionId.innerHTML = `Jion's birthday is ${jionDays.toLocaleString('en-US', {
+  timeZone: 'Australia/Brisbane',
+})} days left.`
